9c9919a8c0ce814d2d6ac4cbb64cdf58acd1f357
AshfoMon <ashfordmonte@gmail.com>
2020-02-28 14:21:57 -0600

    Added questions into an object, object is displayed onto trivia page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,20 +5,126 @@ $(document).ready(function () {
     startButton.addClass("start-button");
     startButton.html("<h3>Start</h3>");
 
-    $(".container-body").append(startButton);
+    $(".start-button").append(startButton);
   }
   startPage();
-  $(".start-button").on("click", function (event) {
-    $(".container-body").empty();
+
+  $(".start-button").click( function (event) {
+    event.stopPropagation();
+    console.log("function called");
+    $(".start-button").empty();
 
     var timerDiv = $("<div>");
     timerDiv.addClass("row justify-content-md-center timerDiv");
     $(".container-body").append(timerDiv);
+
     
+    function buildTrivia() {
+      $(".trivia").css("display", "block");
+      console.log("Build Trivia called");
+      const output = [];
+
+      questionsObject.forEach(
+        (currentQuestion, questionNumber) => {
+          const answers = [];
+
+          for (letter in currentQuestion.answers) {
+            answers.push(
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+                </label>`
+            );
+          }
+          output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join('')} </div>`
+          );
+        }
+      );
+      triviaContent.html(output.join(''));
+    }
+    function showResults() {}
+    
+    const triviaContent = $(".trivia-content");
+    const submitButton = $("#submit");
+
+    const questionsObject = [
+      {
+        question: "Which band is known for 'defining' heavy metal?",
+        answers: {
+          a: "Yes",
+          b: "Genesis",
+          c: "Black Sabbath",
+          d: "The Beatles"
+        },
+        correctAnswer: "c"
+      },
+      {
+        question: "Who was the lead singer for the band Led Zeppelin?",
+        answers: {
+          a: "Robert Plant",
+          b: "John Paul Johns",
+          c: "Jimmy Page",
+          d: "John Bonham"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "Who played bass for the band Yes?",
+        answers: {
+          a: "Rick Wakeman",
+          b: "Chris Squire",
+          c: "Jpn Anderson",
+          d: "Bill Bruford"
+        },
+        correctAnswer: "b"
+      },
+      {
+        question: "Which Beatles album contained the infamous song 'Helter Skelter'?",
+        answers: {
+          a: "The Beatles",
+          b: "Yellow Submarine",
+          c: "Abbey Road",
+          d: "Please Please me"
+        },
+        correctAnswer: "a"
+      },
+      {
+        question: "Who was the original vocalist for AC/DC?",
+        answers: {
+          a: "Angus Young",
+          b: "Ozzy Osbourne",
+          c: "Brian Johnson",
+          d: "Bon Scott"
+        },
+        correctAnswer: "d"
+      },
+    ]
+
+    buildTrivia();
+
+    submitButton.on("click", showResults);
+
+
+
+
     timeStart();
 
 
+
+
+
+
+
+
+
+
+
   })
+
+
 
 
 
